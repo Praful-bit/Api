8d11127b4164460dcc0ebe4f6ada3e3084d352e2
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-05-12 15:16:37 +0530

    loading when we fetch data

--- forApi-call/src/App.jsx
+++ forApi-call/src/App.jsx
@@ -1,40 +1,51 @@
 import { useEffect, useState } from "react";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState(null);
 
-  // by async await 
-  
-  // useEffect(() => {  
-  // const data =async()=>{
-  // const response = await fetch("https://swapi.dev/api/films")
-  // const resData = await response.json()
-  // setData(resData)
-  // }
-  // data()
-  // }, [])
-
-  useEffect(() => {
-    fetch("https://swapi.dev/api/films")
-      .then((res) => res.json())
-      .then((resData) => setData(resData))
-      .catch((err) => console.log(err));
-  }, []);
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films");
+      const resData = await response.json();
+      setData(resData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setIsLoading(false); // Ensure isLoading is set to false regardless of success or failure
+    }
+  };
 
   return (
-    <>
-      <h1>Hello Api</h1>
-      {console.log(data)}
-      {data && (
-        <ul>
-          {data.results.map((film) => (
-            <>
-              <li key={film.episode_id}>{film.title}</li>
-              <li>{film.opening_crawl}</li>
-            </>
-          ))}
-        </ul>
-      )}
-    </>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Hello Api</h1>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={fetchData}
+        >
+          Fetch Data
+        </button>
+        {isLoading && (
+          <div className="mt-4 flex justify-center items-center">
+            <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-500"></div>
+            <p className="ml-2 text-gray-500">Loading...</p>
+          </div>
+        )}
+
+        {data && (
+          <ul>
+            {data.results.map((film) => (
+              <li key={film.episode_id}>
+                <strong>{film.title}</strong>
+                <p>{film.opening_crawl}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 }
 
